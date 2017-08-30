3942ecda2ca6819bb3822d4ecd7fb2c5ee2fd0e1
Nik Nyby <nnyby@columbia.edu>
2017-08-30 11:11:47 -0400

    Update contact email

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name="django-sorlimageblock",
     version="0.1.0",
     author="Columbia University's Center for Teaching and Learning",
-    author_email="ColumbiaCTL@columbia.edu",
+    author_email="ccnmtl-dev@columbia.edu",
     url="https://github.com/ccnmtl/django-sorlimageblock",
     description="sorl-ified ImageBlock for django-pagetree.",
     install_requires=[
